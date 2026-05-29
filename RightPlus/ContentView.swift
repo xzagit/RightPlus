d4ec8c93e8e1da3c03b7eb1d52ccc05b8c8a933b
xuziao <750133671@qq.com>
2026-05-29 10:15:04 +0800

    Initial Commit

---
+++ RightPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RightPlus
+//
+//  Created by 徐子傲 on 2026/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RightPlus/RightPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightPlusApp.swift
+//  RightPlus
+//
+//  Created by 徐子傲 on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct RightPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
